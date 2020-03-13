a3c34479ac1c2c2437bc1dc0ec8a85960de77a8e
lvwanyou <lvwanyou@163.com>
2020-03-14 00:41:24 +0800

    通过率脚本完成
    1. 将模糊生成数据放入： Data/mqtt_template.txt中；
    2. 启动发送脚本： FrameSenderReciver_A.py
    3.详细发送/接收数据包 log 文件会存入MQTT/log_data_communications/logfirst_‘time’.txt  中； 在txt最下方会有通过率计算结果

--- MQTT/mqtt_data_transmission/FrameSenderReciver_A.py
+++ MQTT/mqtt_data_transmission/FrameSenderReciver_A.py
@@ -3,8 +3,15 @@ from MQTT.mqtt_data_transmission.utils.handle_data_util import dataSwitch
 import time
 import os
 import sys
+import subprocess
+import signal
 from tqdm import tqdm
+
 if __name__ == '__main__':
+    py_location = "MQTT/mqtt_data_transmission/FrameSenderReciver_B.py"
+    # FrameSenderReciver_before_pid = int(subprocess.run(['pgrep', '-f', py_location]).strip())
+    # os.kill(FrameSenderReciver_before_pid, signal.SIGTERM)
+    global sum_count
     TCP_IP = '127.0.0.1'
     TCP_PORT = 1883
     BUFFER_SIZE = 10000
@@ -12,66 +19,80 @@ if __name__ == '__main__':
     s.setblocking(0)
     s.settimeout(2)
     s.connect((TCP_IP, TCP_PORT))
-    logs = []
 
-    # count 为 两个脚本发送脚本共有的index
+
+
+    """
+    count 为 两个脚本发送脚本共有的index;
+    log_file_name 为两个脚本共同保存log 的文件名;
+    error_count 为两个脚本共享的错误的log的个数;
+    error_list 为两个脚本共享的log list
+    """
     if len(sys.argv) > 1:
-        count = int(sys.argv[1]) ##############################################################################################
+        count = int(sys.argv[1])
+        log_file_name = str(sys.argv[2])
+        error_count = int(sys.argv[3])
+        error_list = str(sys.argv[4]).split('.')
     else:
         count = 0
-    #with open('GeneratedDataModbus/generated_data_write_single_register_16.txt', 'r') as f:
-    #with open('dataseven1.txt', 'r') as f:
-    with open('Data/mqtt_template.txt', 'r') as f:
-        try:
-            content = f.readlines()
-            req_val = "104100044d51545404e6870000147061686f3131323432353032373331383130303000036c7774000b48656c6c6f20576f726421000561646d696e0006313233343536"
-            req_string = dataSwitch(req_val.strip('\n'))  # switch hex to bit for sending it to the simulations
-            s.send(req_string)
-            for i in tqdm(range(len(content))):
-                if i >= count:
-                    val = content[i]
-                    if val is not None and val != '\n':
-                        val = val.strip()
-                        count = count + 1
-                        string = dataSwitch(val.strip('\n'))    # switch hex to bit for sending it to the simulations
-                        s.send(string)
-                        logs.append("No." +str(i) + ' TX :' + val)
-                        time.sleep(0.1)
-                        data = s.recv(BUFFER_SIZE)
+        log_file_name = "MQTT/log_data_communications/logfirst_" + time.strftime("%H.%M.%S#%Y-%m-%d",
+                                                                                 time.localtime()) + ".txt"
+        error_count = 0
+        error_list = []
 
-                        # result = data.encode('hex')########################################################################
-                        result = data.hex()
-                        logs.append('\n')
-                        logs.append("==> No." + str(i) + ' RX :' + result + '\n')
-                        logs.append('\n')
-        except IOError as e:
-            s.close()
-            f.close()
-            os.system("python MQTT/mqtt_data_transmission/FrameSenderReciver_B.py " + str(count))
-            print('can not read the file!')
+    with open(log_file_name,
+              "a") as f_log:
+        try:
+            with open('Data/mqtt_template.txt', 'r') as f:
+                try:
+
+                    content = f.readlines()
+                    req_val = "104100044d51545404e6870000147061686f3131323432353032373331383130303000036c7774000b48656c6c6f20576f726421000561646d696e0006313233343536"
+                    req_string = dataSwitch(req_val.strip('\n'))  # switch hex to bit for sending it to the simulations
+                    s.send(req_string)
+                    sum_count = len(content)
+                    for i in tqdm(range(len(content))):
+                        if i >= count:
+                            val = content[i]
+                            if val is not None and val != '\n':
+                                val = val.strip()
+                                count = count + 1
+                                string = dataSwitch(val.strip('\n'))    # switch hex to bit for sending it to the simulations
+                                s.send(string)
+                                f_log.write(" No." +str(i) + ' TX :' + val)
+
+                                # time.sleep(0.1)
+                                data = s.recv(BUFFER_SIZE)
+
+                                # result = data.encode('hex')########################################################################
+                                result = data.hex()
+                                f_log.write('\n')
+                                f_log.write("==> No." + str(i) + ' RX :' + result + '\n')
+                                f_log.write('\n')
+                except IOError as e:
+                    s.close()
+                    f.close()
+                    error_count += 1
+                    error_list.append(val)
+                    f_log.write('\n error! **********************************************  \n')
+                    f_log.write('\n')
+                    f_log.close()
+                    print('No. ' + str(i) + " go wrong; content: " + val)
+                    error_list = '.'.join(error_list)
+                    os.system("python " + py_location + " %i %s %i %s" % (count, log_file_name, error_count, error_list))
+                    exit()
+                finally:
+                    if count >= sum_count:
+                        log_sum = "Error ratio :" + str(error_count) + "/" + str(sum_count) + ";  Acceptance : {:.3%}".format(
+                            error_count / sum_count)
+                        print(log_sum)
+                        f_log.write(log_sum)
+        except:
+            None
         finally:
-            with open("MQTT/log_data_communications/logfirst_0.txt", "a") as f:
-                f.write(" ".join(logs))
+            f_log.close()
+
 #RX Reciving Data
 #TX Sending Data
 #RX Reciving Data
 #TX Sending Data
-#RX Reciving Data
-#TX Sending Data
-#RX Reciving Data
-#TX Sending Data
-#RX Reciving Data
-#TX Sending Data
-#RX Reciving Data
-#TX Sending Data
-#RX Reciving Data
-#TX Sending Data
-#RX Reciving Data
-#TX Sending Data
-#RX Reciving Data
-#TX Sending Data
-#RX Reciving Data
-#TX Sending Data
-#RX Reciving Data
-#TX Sending Data
-#RX Reciving Data
